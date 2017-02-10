6cddd7550a115a2e584b1b32301ff34b704372c6
dr president <drpresident@localhost.localdomain>
2017-02-10 10:57:14 -0600

    added a class for async requests

--- async.py
+++ async.py
@@ -1,9 +1,35 @@
 from threading import Thread
 
-def async_request(request_func, 
-        success_handler=None, 
-        error_handler=None, 
-        args=None):
+class Async:
+    def __init__(self):
+        self.threads = list()
+        self.daemons = list()
 
-    th = Thread(target=request_func, args=(args if args else None))
+    def __del__(self):
+        for t in self.threads:
+            t.join()
+
+    def start_daemon(self, func, args=None):
+        d = Thread(target=func,args=args)
+        d.setDaemon(True)
+        d.start()
+        self.daemons.append(d)
+
+    def async_request(request_func, 
+            success_handler=None, 
+            error_handler=None, 
+            args=None):
+
+        def runner():
+            try:
+                response = request_func(*args if args else None)
+            except:
+                error_handler(response)
+                return
+
+            success_handler(response)
+
+        th = Thread(target=runner)
+        self.threads.append(th)
+        th.start()
 
